9abdd69fa26bec451574bcf3094cc13d8c2cfdfc
KwonByeongWoo <kbw0178@naver.com>
2025-07-25 10:46:10 +0900

    최초 커밋

--- Calculator.java
+++ Calculator.java
@@ -1,15 +1,10 @@
-// 작업1
+// 작업2
 public class Calculator {
     public static void main(String[] args) {
         System.out.println("1+1 = 2");
         System.out.println("1-1 = 0");
         System.out.println("2*4 = 8");
         System.out.println("4/2 = 2");
-        System.out.println("Hello AI");
-        System.out.println("Hello AII");
-        System.out.println("Hello AIII");
-        System.out.println("Hello AIIII");
-        System.out.println("Hello AIIIIII");
-        System.out.println("Hello AIIIIIII");
+        // 완료1
     }
 }
